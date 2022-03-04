ec8ae4f93d0b3c87e9e712f81fea36804ede1a31
iascn <99797054+iascn@users.noreply.github.com>
2022-03-04 10:03:28 +0800

    Update fofa.py

--- fofa.py
+++ fofa.py
@@ -5,9 +5,9 @@ import time
 import sys
 
 '''
-@author : xxx 3.1
+@author : xxx 4.1
 更改before 日期
-更改cookie
+更改cookie !!!!
 python3 Fofa.py fofa语句
 e.g. ：python3 Fofa.py 'app="Apache_OFBiz"'
 '''
@@ -18,12 +18,19 @@ e.g. ：python3 Fofa.py 'app="Apache_OFBiz"'
 headers={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:91.0) Gecko/20100101 Firefox/91.0"}
 url = "https://fofa.info/result?qbase64="
 #1. 需要更改时间
-after = "2020-01-01"
-before = "2022-01-13"
+after = "2021-01-01"
+before = "2022-03-04"
 #search = '"ValidateLogin" && title=="后台登录"'
 search = sys.argv[1]
 #2. 需要更改cookie
 cookies = {""}
+
+
+'''
+参考下面这个....
+
+cookies = {"Hm_lvt_b5514a35664fd4ac6a893a1e56956c97":"xxx","Hm_lpvt_b5514a35664fd4ac6a893a1e56956c97":"xx","isUpgrade":"","refresh_token":"xxx","befor_router":"","fofa_token":"xxx","user":"xxx"}
+'''
 
 
 #return - str_data
@@ -55,24 +62,25 @@ def timc(data):
 def crl(url,search,after,before):
     stxt = search+' && after="{0}" && before="{1}"'.format(after,before)
     url = url + str(base64.b64encode(stxt.encode('utf-8')))[2:-1]
-    return url
+    print(url)
+    return(url)
 
 f = open('url.txt','w')
 
 def num(url):
-    a = requests.get(url,cookies=cookies,timeout=10)
-    n1 = re.findall('<li class="number">(\d)</li>',a.text)
-    print('all page '+str(int(n1[-1])-1))
-    for i in n1:
-        i = str(int(i)-1)
-        stxt = url + '&page='+i+'&page_size=10'
+    for i in range(1,4):
+        i = str(i)
+        last = 20
+        if i == "3":
+            last = 10
+        stxt = url + '&page='+i+'&page_size='+str(last)
         print('page '+i)
         a = requests.get(stxt,cookies=cookies,timeout=10)
         #c2 = re.findall('(http://.*?)"',a.text)
         #c1 = re.findall('>(https://.*?)<',a.text)
         c = re.findall('<span class="aSpan"><a href="(.*?)" target="_blank">',a.text)
         #c = c1 + c2
-        if i == str(int(n1[-1])-1):
+        if (i == "3"):
             time = re.findall('<span>(\d*?-\d*?-\d*?)</span>',a.text)
             print('                             stop in => '+time[-1])
         for k in c:
